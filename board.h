6f58722e5349d6314fd5a0372e3f834818168d88
Aengus_Roberts <asr66@bath.ac.uk>
2023-02-24 21:45:12 +0000

    Initial commit

---
+++ board.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class Board {
+private:
+public:
+    // Defines which players turn it is: white (0), black(1)
+    int player;
+
+    // states to check whether a player is in check
+    int whiteCheck;
+    int blackCheck;
+
+    // Constructor
+    Board();
+
+    // Destructor
+    ~Board();
+
+    void U
+};
